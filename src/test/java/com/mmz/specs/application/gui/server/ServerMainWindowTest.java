662e12da996575aba2e506c30ce3f6431bfb9a31
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-02-15 09:21:52 +0300

    Added junit4 tests Added test for serverOnlineString

---
+++ src/test/java/com/mmz/specs/application/gui/server/ServerMainWindowTest.java
@@ -0,0 +1,25 @@
+package com.mmz.specs.application.gui.server;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ServerMainWindowTest {
+
+
+    @Test
+    public void getServerOnlineString() {
+        ServerMainWindow serverMainWindow = new ServerMainWindow();
+        serverMainWindow.onlineSeconds = 25 * 60 * 60; // 25 часов == 1 день 1 час :00
+        String expected = "1д. 1ч. 0м. 0с.";
+        assertEquals(expected, serverMainWindow.getServerOnlineString());
+
+        serverMainWindow.onlineSeconds = 30 * 24 * 60 * 60 + 20 * 60 * 60 + 3 * 60 + 10; // 30 дней, 20 часов, 3 минуты, 10 сек
+        expected = "30д. 20ч. 3м. 10с.";
+        assertEquals(expected, serverMainWindow.getServerOnlineString());
+
+        serverMainWindow.onlineSeconds = 1600 * 24 * 60 * 60 + 2 * 60 * 60 + 33 * 60 + 2; // 1600 дней, 2 часа, 33 минуты, 2 сек
+        expected = "1600д. 2ч. 33м. 2с.";
+        assertEquals(expected, serverMainWindow.getServerOnlineString());
+    }
+}
