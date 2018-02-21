bd354c71a46b522617a521bd1738dca59050bfba
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-02-21 08:44:46 +0300

    Added SERVER_DEFAULT_SOCKET_PORT

--- src/main/java/com/mmz/specs/application/core/server/ServerConstants.java
+++ src/main/java/com/mmz/specs/application/core/server/ServerConstants.java
@@ -2,4 +2,7 @@ package com.mmz.specs.application.core.server;
 
 public class ServerConstants {
     public static final String SERVER_SETTINGS_FILE_LOCATION_KEY = "specs.db.connection.settings.file";
+
+
+    public static final int SERVER_DEFAULT_SOCKET_PORT = 43745;
 }
