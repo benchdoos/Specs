b3ede35a11d7e44c2bb87eca66e72fa6bfbf7a00
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-05-24 15:04:29 +0300

    Gui improvements

--- src/main/java/com/mmz/specs/application/gui/client/ClientMainWindow.java
+++ src/main/java/com/mmz/specs/application/gui/client/ClientMainWindow.java
@@ -621,15 +621,14 @@ public class ClientMainWindow extends JFrame {
         contentPane = new JPanel();
         contentPane.setLayout(new GridLayoutManager(2, 2, new Insets(0, 0, 0, 0), -1, -1));
         clientMainTabbedPane = new JTabbedPane();
+        clientMainTabbedPane.setTabLayoutPolicy(1);
         contentPane.add(clientMainTabbedPane, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         clientMainTabbedPane.addTab("Главная", mainPanel);
-        final JScrollPane scrollPane1 = new JScrollPane();
-        mainPanel.add(scrollPane1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(5, 2, new Insets(0, 0, 0, 0), -1, -1));
-        scrollPane1.setViewportView(panel1);
+        mainPanel.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         panel1.setBorder(BorderFactory.createTitledBorder("Функционал"));
         viewDetailListButton = new JButton();
         viewDetailListButton.setAutoscrolls(false);
