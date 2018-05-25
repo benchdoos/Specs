c76556aeb3685154939aa8b1217482b3dd834840
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-05-25 10:15:01 +0300

    Added build number info

--- src/main/java/com/mmz/specs/application/gui/client/ClientMainWindow.java
+++ src/main/java/com/mmz/specs/application/gui/client/ClientMainWindow.java
@@ -50,6 +50,7 @@ import java.awt.event.*;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 
 public class ClientMainWindow extends JFrame {
@@ -68,6 +69,7 @@ public class ClientMainWindow extends JFrame {
     private JButton editDataButton;
     private JButton noticeListViewButton;
     private JButton adminButton;
+    private JTextField applicationVersionArea;
     private Timer uiUpdateTimer;
 
     public ClientMainWindow() {
@@ -400,6 +402,27 @@ public class ClientMainWindow extends JFrame {
         initListeners();
         initKeyBindings();
         unlockButtonIconUpdate(false);
+
+        initApplicationVersionArea();
+    }
+
+    private void initApplicationVersionArea() {
+        applicationVersionArea.setBorder(BorderFactory.createEmptyBorder());
+        Properties properties = new Properties();
+        try {
+            properties.load(ClientMainWindow.class.getResourceAsStream("/application.properties"));
+            String name = properties.getProperty("application.name");
+            String version = properties.getProperty("application.version");
+            String build = properties.getProperty("application.build");
+            if (version != null && build != null) {
+                applicationVersionArea.setText(name + " v." + version + " (" + build + ")");
+            } else {
+                applicationVersionArea.setText(null);
+                applicationVersionArea.setVisible(false);
+            }
+        } catch (Exception e) {
+            log.warn("Could not load application version info", e);
+        }
     }
 
     private void onOpenFileMenu() {
@@ -694,11 +717,16 @@ public class ClientMainWindow extends JFrame {
         final Spacer spacer3 = new Spacer();
         panel2.add(spacer3, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JPanel panel3 = new JPanel();
-        panel3.setLayout(new GridLayoutManager(1, 1, new Insets(0, 4, 0, 0), -1, -1));
+        panel3.setLayout(new GridLayoutManager(1, 2, new Insets(0, 4, 0, 0), -1, -1));
         panel2.add(panel3, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         statusLabel = new JLabel();
         statusLabel.setIcon(new ImageIcon(getClass().getResource("/img/gui/status/red12.png")));
         panel3.add(statusLabel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        applicationVersionArea = new JTextField();
+        applicationVersionArea.setBackground(new Color(-855310));
+        applicationVersionArea.setEditable(false);
+        applicationVersionArea.setForeground(new Color(-7697782));
+        panel3.add(applicationVersionArea, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(250, -1), null, 0, false));
         final JPanel panel4 = new JPanel();
         panel4.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
         panel2.add(panel4, new GridConstraints(0, 2, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
