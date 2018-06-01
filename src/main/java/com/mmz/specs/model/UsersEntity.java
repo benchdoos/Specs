e0c0a9f3877cdc654db177bbad00a5f5ad547df8
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-06-01 13:27:56 +0300

    Added todo

--- src/main/java/com/mmz/specs/model/UsersEntity.java
+++ src/main/java/com/mmz/specs/model/UsersEntity.java
@@ -149,6 +149,20 @@ public class UsersEntity implements Comparable<UsersEntity> {
 
     @Override
     public boolean equals(Object other) {
+        //todo add equalsBuilder
+        /*//example:
+        if (obj == null) { return false; }
+        if (obj == this) { return true; }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        MyClass rhs = (MyClass) obj;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(obj))
+                .append(field1, rhs.field1)
+                .append(field2, rhs.field2)
+                .append(field3, rhs.field3)
+                .isEquals();*/
         if (this == other) return true;
         if (!(other instanceof UsersEntity)) return false;
         final UsersEntity that = (UsersEntity) other;
