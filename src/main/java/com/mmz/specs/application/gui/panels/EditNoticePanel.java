d683035c2ab5c3b23513ef4e6aef3e9f1268e790
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-06-19 00:19:05 +0300

    Fixed mainTree

--- src/main/java/com/mmz/specs/application/gui/panels/EditNoticePanel.java
+++ src/main/java/com/mmz/specs/application/gui/panels/EditNoticePanel.java
@@ -506,11 +506,9 @@ public class EditNoticePanel extends JPanel implements AccessPolicy, Transaction
     }
 
     private void updateTreeDetail() {
-        TreePath selectionPath = mainTree.getSelectionPath();
         DefaultTreeModel model = (DefaultTreeModel) mainTree.getModel();
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) mainTree.getLastSelectedPathComponent();
         model.reload(node);
-        mainTree.expandPath(selectionPath);
     }
 
 
