55d42e18951a53dddf6dbff1c91b473a60330279
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-09-25 22:21:43 +0300

    Fixed file creation check

--- src/main/java/com/mmz/specs/io/SPTreeIOManager.java
+++ src/main/java/com/mmz/specs/io/SPTreeIOManager.java
@@ -17,39 +17,54 @@ package com.mmz.specs.io;
 
 import com.mmz.specs.application.gui.common.utils.managers.ProgressManager;
 import com.mmz.specs.application.utils.Logging;
+import com.mmz.specs.connection.ServerDBConnectionPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 
 import java.io.File;
 import java.io.IOException;
 
+import static com.mmz.specs.application.core.ApplicationConstants.APPLICATION_EXPORT_FOLDER_LOCATION;
+
 public class SPTreeIOManager implements IOManager {
     private static final Logger log = LogManager.getLogger(Logging.getCurrentClassName());
+    private final Session session;
 
     private ProgressManager progressManager;
     private boolean isInterrupted;
 
     public SPTreeIOManager(ProgressManager progressManager) {
         this.progressManager = progressManager;
+        this.session = ServerDBConnectionPool.getInstance().getSession();
     }
 
     @Override
     public void exportData(File file) throws IOException {
         log.info("Starting export of tree (SPT) to file: {}", file);
-        log.debug("Checking ability to create file: {}", file);
-        progressManager.setText("Проверка доступности файла");
-        if (!file.canWrite()) {
-            log.warn("Can not create file: {}", file);
-            throw new IOException("Can not write data to file: " + file);
-        }
+        checkCreate(file);
 
-        createTreeJSON();
+        createTreeJSON(file);
         progressManager.setTotalProgress(1);
 
     }
 
-    private void createTreeJSON() {
-        
+    private void checkCreate(File file) throws IOException {
+        log.debug("Checking ability to create file: {}", file);
+        progressManager.setText("Проверка доступности файла");
+
+        new File(APPLICATION_EXPORT_FOLDER_LOCATION).mkdirs();
+
+        if (!file.createNewFile()) {
+            log.warn("Can not create file: {}", file);
+            throw new IOException("Can not write data to file: " + file);
+        } else {
+            file.delete();
+        }
+    }
+
+    private void createTreeJSON(File file) {
+
     }
 
     @Override
