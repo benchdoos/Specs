b43a0dbf40e66f3019ea7a0ff9b56db8f03a25ad
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2018-09-27 23:58:25 +0300

    Added streams closing

--- src/main/java/com/mmz/specs/application/managers/CommonSettingsManager.java
+++ src/main/java/com/mmz/specs/application/managers/CommonSettingsManager.java
@@ -28,16 +28,19 @@ public class CommonSettingsManager {
     public static String getServerSettingsFilePath() throws IOException {
         String result;
         Properties properties = new Properties();
-        properties.loadFromXML(new FileInputStream(ApplicationConstants.SETTINGS_FILE_PATH));
-        result = properties.getProperty(ServerConstants.SERVER_SETTINGS_FILE_LOCATION_KEY);
+        try (final FileInputStream in = new FileInputStream(ApplicationConstants.SETTINGS_FILE_PATH)) {
+            properties.loadFromXML(in);
+            result = properties.getProperty(ServerConstants.SERVER_SETTINGS_FILE_LOCATION_KEY);
+        }
         return result;
     }
 
     public static void setServerSettingsFilePath(String path) throws IOException {
         Properties properties = new Properties();
         properties.setProperty(ServerConstants.SERVER_SETTINGS_FILE_LOCATION_KEY, path);
-        properties.storeToXML(new FileOutputStream(ApplicationConstants.SETTINGS_FILE_PATH),
-                ApplicationConstants.INTERNAL_FULL_NAME + " settings file locations",
-                ApplicationConstants.DEFAULT_FILE_ENCODING);
+        try (final FileOutputStream os = new FileOutputStream(ApplicationConstants.SETTINGS_FILE_PATH)) {
+            properties.storeToXML(os, ApplicationConstants.INTERNAL_FULL_NAME + " settings file locations",
+                    ApplicationConstants.DEFAULT_FILE_ENCODING);
+        }
     }
 }
